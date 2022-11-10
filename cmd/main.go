da05e273713e7bd858e83a950c6e32bd8b6ae688
Pavle Portic <git@theedgeofrage.com>
2022-11-10 15:35:52 +0100

    Implement suggestions

--- cmd/main.go
+++ cmd/main.go
@@ -15,17 +15,19 @@ func main() {
 	queryID := flag.Int("q", 0, "The ID of the query to execute. Conflicts with -e")
 	queryParametersStr := flag.String("p", "{}", "Parameters to pass to the query in JSON format")
 	executionID := flag.String("e", "", "ID of an existing execution to check status. Conflicts with -q")
-	maxRetries := flag.Int("max-retries", 5, "Max number of get errors tolerated before giving up")
+	maxRetries := flag.Int("max-retries", 5, "Max number of errors tolerated before giving up")
 	pollInterval := flag.Duration("poll-interval", 5*time.Second, "Interval in seconds for polling for results")
 
 	flag.Parse()
 
-	if (*executionID == "") == (*queryID == 0) {
+	// Guards against providing either both a query and execution ID or neither.
+	if (*executionID == "" && *queryID == 0) || (*executionID != "" && *queryID != 0) {
 		fmt.Fprintln(os.Stderr, "must provide exactly one of ExecutionID and QueryID")
 		os.Exit(1)
 	}
 
-	env, err := config.ParseEnv()
+	// Load the API key config from the DUNE_API_KEY environment variable
+	env, err := config.FromEnvVars()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
@@ -51,6 +53,8 @@ func main() {
 		execution = dune.NewExecution(client, *executionID)
 	}
 
+	// An Execution object provides an interface for getting its state, results,
+	// cancelling it, or blocking until it completes, which is what WaitGetResults does
 	result, err := execution.WaitGetResults(*pollInterval, *maxRetries)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "failed to retrieve results:", err)
