0fe587b34401a85560326d08011c9d9ca01a2fea
Ivan Pusic <450140+ivpusic@users.noreply.github.com>
2026-03-03 15:06:02 +0100

    cleanup

--- dune/dune.go
+++ dune/dune.go
@@ -463,60 +463,6 @@ func (c *duneClient) getUsage(startDate, endDate *string) (*models.UsageResponse
 	return &usageResp, nil
 }
 
-func (c *duneClient) ListDatasets(
-	limit, offset int, ownerHandle, datasetType string,
-) (*models.ListDatasetsResponse, error) {
-	listURL := fmt.Sprintf(listDatasetsURLTemplate, c.env.Host)
-
-	params := fmt.Sprintf("?limit=%d&offset=%d", limit, offset)
-	if ownerHandle != "" {
-		params += fmt.Sprintf("&owner_handle=%s", url.QueryEscape(ownerHandle))
-	}
-	if datasetType != "" {
-		params += fmt.Sprintf("&type=%s", url.QueryEscape(datasetType))
-	}
-
-	req, err := http.NewRequest("GET", listURL+params, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	resp, err := httpRequest(c.env.APIKey, req)
-	if err != nil {
-		return nil, err
-	}
-
-	var datasetsResp models.ListDatasetsResponse
-	decodeBody(resp, &datasetsResp)
-	if err := datasetsResp.HasError(); err != nil {
-		return nil, err
-	}
-
-	return &datasetsResp, nil
-}
-
-func (c *duneClient) GetDataset(slug string) (*models.DatasetResponse, error) {
-	getURL := fmt.Sprintf(getDatasetURLTemplate, c.env.Host, slug)
-
-	req, err := http.NewRequest("GET", getURL, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	resp, err := httpRequest(c.env.APIKey, req)
-	if err != nil {
-		return nil, err
-	}
-
-	var datasetResp models.DatasetResponse
-	decodeBody(resp, &datasetResp)
-	if err := datasetResp.HasError(); err != nil {
-		return nil, err
-	}
-
-	return &datasetResp, nil
-}
-
 func (c *duneClient) ListUploads(limit, offset int) (*models.UploadsListResponse, error) {
 	listURL := fmt.Sprintf(listUploadsURLTemplate, c.env.Host)
 
